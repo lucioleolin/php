1c554339093812bb7995b15fef8d16597aa7f304
lucioleolin <125644692+lucioleolin@users.noreply.github.com>
2025-02-25 00:24:13 +0800

    Add files via upload

---
+++ ch1.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title> Welcome to my first practice 
+</title>
+    </head>
+
+    <body>
+        we will learn about php, MySQL and AJAX
+
+        <?php echo "<br>Hope this semester all pass!";
+
+        $i = 5;
+        $j = $i + 1;
+        echo $j;
+
+    ?>
+    </body>
+</html>
+    
